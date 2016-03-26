bcc2e6ffdee7f58e7f1b13b757487154c9eff217
dppower <powerdp@gmail.com>
2016-03-26 14:06:56 +0000

    Broken implementation without content projection.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,25 +1,46 @@
-import {Component, OnInit, ViewChild, AfterViewChecked} from "angular2/core";
+import {Component, OnInit, ViewChild, AfterViewChecked, ChangeDetectorRef} from "angular2/core";
 import {AsyncPipe} from "angular2/common";
 import {Subject, Observable} from "rxjs/Rx";
-import {AutoScrollComponent} from "./auto-scroll.component";
+import {AutoscrollDirective} from "./auto-scroll.directive";
 
 @Component({
     selector: 'my-app',
     template: `
-    <auto-scroll-display>
+    <div #this class="chat-box" [inScrollHeight]="this.scrollHeight" [inClientHeight]="this.clientHeight" autoScroll (scrollEvent)="shouldScroll = $event">
         <p *ngFor="#message of chat$ | async">{{message}}</p>
-    </auto-scroll-display>
+    </div>
     <div>
         <button type="button" (click)="addNewMessage()">New Message</button>
         <span>Count: {{count}}</span>
     </div>
     `,
-    directives: [AutoScrollComponent]
+    styles: [`
+    .chat-box {
+        height: 200px;
+        width: 400px;
+        overflow-y: scroll;
+    }
+    `],
+    directives: [AutoscrollDirective]
 })
 export class AppComponent implements OnInit {
     chatSubject: Subject<string> = new Subject<string>();
     chat$: Observable<string[]>;
     count: number = 0;
+
+    @ViewChild(AutoscrollDirective) autoScroll: AutoscrollDirective;
+    shouldScroll: boolean = false;
+
+    constructor(private cdr_: ChangeDetectorRef) { };
+
+    ngAfterViewChecked() {
+        if (this.shouldScroll) {
+            //setTimeout(() => this.autoScroll.scroll(), 0);
+            this.autoScroll.scroll();
+            this.cdr_.detectChanges();
+            this.shouldScroll = false;
+        }
+    }
 
     addNewMessage() {
         this.count++;
